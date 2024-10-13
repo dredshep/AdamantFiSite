61cbb38c3cb326579a47e5a960c6636031bccd08
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-10-13 08:46:40 -0400

    simplify fetching Pairs

---
+++ pages/api/getFactoryPairs.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { queryFactoryPairs } from "@/utils/apis/factoryPairs";
+import { handleApiError } from "@/utils/apis/handleApiError";
+
+export default async function getFactoryPairs(
+  _req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const data = await queryFactoryPairs();
+    res.status(200).json(data);
+  } catch (error) {
+    handleApiError(error, res);
+  }
+}
