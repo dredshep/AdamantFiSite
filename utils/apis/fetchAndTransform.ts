0c3130445ec9c23c56ded33832c213de303dc710
dredshep <dredshep@gmail.com>
2024-10-14 13:47:45 +0200

    chore: fixed type inaccuracy & bumped dependencies

--- utils/apis/fetchAndTransform.ts
+++ utils/apis/fetchAndTransform.ts
@@ -1,6 +1,7 @@
 export async function fetchAndTransform<T>(
   url: string,
-  transformer: (data: unknown) => T = (data) => data as T
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  transformer: (data: any) => T = (data) => data as T
 ): Promise<T> {
   const response = await fetch(url);
   const data = await response.json();
