6c5d690da9cfdc088ed274c8667cb9f1f4a0b358
dredshep <dredshep@gmail.com>
2024-12-21 17:42:23 +0100

    refactor(PoolTokenInput): enhance input handling with loading state and estimated price
    - Added optional isLoading prop to PoolTokenInput for better user feedback during token input.
    - Introduced estimated price display with a default value of "0" to improve clarity for users.
    - Cleaned up commented-out code related to balance handling and max click functionality for better maintainability.
    
    These changes improve the user experience by providing clearer input states and reducing clutter in the code.

--- components/app/Shared/Forms/Input/PoolTokenInput/index.tsx
+++ components/app/Shared/Forms/Input/PoolTokenInput/index.tsx
@@ -2,22 +2,22 @@ import TokenInputBase from '@/components/app/Shared/Forms/Input/TokenInputBase';
 import { usePoolStore } from '@/store/forms/poolStore';
 import { PoolTokenInputs, SecretString } from '@/types';
 import React from 'react';
-// import InputLabel from "@/components/app/Shared/Forms/Input/InputLabel";
 
 interface PoolTokenInputProps {
   poolInputIdentifier: keyof PoolTokenInputs;
   token: {
     symbol: string;
-    // balance: number;
     address: SecretString;
   };
   label: string;
+  isLoading?: boolean;
 }
 
 const PoolTokenInput: React.FC<PoolTokenInputProps> = ({
   poolInputIdentifier: inputIdentifier,
   token,
   label,
+  isLoading = false,
 }) => {
   const { tokenInputs, setTokenInputAmount } = usePoolStore();
 
@@ -25,23 +25,19 @@ const PoolTokenInput: React.FC<PoolTokenInputProps> = ({
     setTokenInputAmount(inputIdentifier, value);
   };
 
-  // const handleMaxClick = () => {
-  //   setTokenInputAmount(inputIdentifier, token.balance.toString());
-  // };
-
   return (
     <div>
-      {/* <InputLabel label={label} caseType="normal-case" /> */}
       <TokenInputBase
         inputValue={tokenInputs[inputIdentifier]?.amount ?? ''}
         onInputChange={handleInputChange}
         tokenSymbol={token.symbol}
         tokenAddress={token.address}
-        // balance={token.balance.toString()}
-        // onMaxClick={handleMaxClick}
         inputIdentifier={inputIdentifier}
         hasMax={true}
         label={label}
+        showEstimatedPrice={false}
+        estimatedPrice="0"
+        isLoading={isLoading}
       />
     </div>
   );
