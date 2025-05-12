423f8a3db1b52aa654f8d8dd9f665bde834add0d
dredshep <dredshep@gmail.com>
2025-05-13 00:35:09 +0200

    refactor: simplify swap utilities
    - Update fetchSwappableTokens to use config tokens
    - Remove unused estimate.ts with complex path finding logic

--- utils/swap.ts
+++ utils/swap.ts
@@ -1,10 +1,11 @@
-import { ApiToken, getApiToken } from '@/utils/apis/getSwappableTokens';
+import { ConfigToken } from '@/config/tokens';
+import { getSwappableTokens } from '@/utils/apis/getSwappableTokens';
 // import { getTinyChartData } from "@/utils/apis/getTinyChartData";
 // import { SwappableToken } from "@/types/Token";
 // import { TokenInputState } from "@/types";
 
-export const fetchSwappableTokens = async (): Promise<ApiToken[]> => {
-  return await getApiToken();
+export const fetchSwappableTokens = (): ConfigToken[] => {
+  return getSwappableTokens();
 };
 
 // export const fetchChartData = async (tokenAddress: string) => {
