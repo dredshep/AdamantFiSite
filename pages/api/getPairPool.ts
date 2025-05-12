a43deb1cd7a76d1d18efe516cd9020b2847d3807
dredshep <dredshep@gmail.com>
2025-05-13 00:35:08 +0200

    refactor: update API endpoints to use ConfigToken
    - Add better validation for contract_addr parameter
    - Use SecretString type for contract_addr

--- pages/api/getPairPool.ts
+++ pages/api/getPairPool.ts
@@ -1,29 +1,32 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { handleApiError } from "@/utils/apis/handleApiError";
-import { queryPool } from "@/utils/apis/getPairPool";
+import { SecretString } from '@/types';
+import { queryPool } from '@/utils/apis/getPairPool';
+import { handleApiError } from '@/utils/apis/handleApiError';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function getPoolInfo(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { contract_addr } = req.query;
+export default async function getPoolInfo(req: NextApiRequest, res: NextApiResponse) {
+  const { contract_addr } = req.query as { contract_addr: SecretString };
 
   if (
     typeof contract_addr === undefined ||
     (Array.isArray(contract_addr) && contract_addr.length === 0) ||
-    typeof contract_addr !== "string" ||
+    typeof contract_addr !== 'string' ||
     contract_addr.length === 0 ||
     (Array.isArray(contract_addr) &&
       contract_addr.length === 1 &&
-      typeof contract_addr[0] === "string" &&
+      typeof contract_addr[0] === 'string' &&
       contract_addr[0].length === 0)
   ) {
-    return res
-      .status(400)
-      .json({ error: "contract_addr query parameter is required" });
+    return res.status(400).json({ error: 'contract_addr query parameter is required' });
   }
 
   try {
+    if (contract_addr.length === 0) {
+      return res.status(400).json({ error: 'contract_addr query parameter is required' });
+    }
+    // if it doesnt start with secret1
+    if (!contract_addr.startsWith('secret1')) {
+      return res.status(400).json({ error: 'contract_addr must start with secret1' });
+    }
     const data = await queryPool(contract_addr);
     res.status(200).json(data);
   } catch (error) {
