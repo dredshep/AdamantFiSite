7e87a62f8dbe7b6a4fe0cc97302102fa7ca2628d
dredshep <dredshep@gmail.com>
2025-07-03 04:03:34 +0200

    chore: clean up document metadata and remove unused assets
    - Refactored _document.tsx to adjust import order and comment out unused link tags for icons and manifest.
    - Deleted obsolete image files and configuration related to browser and favicon assets to streamline the project structure.
    - Introduced a new SVG favicon for improved visual representation.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
@@ -14,27 +14,12 @@ export default function Document() {
         />
         <meta name="author" content="Adamant Finance" />
 
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0c0a23" />
-        <meta name="msapplication-TileColor" content="#0c0a23" />
-        <meta name="theme-color" content="#ffffff" />
+        {/* <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="AdamantFi" />
+        <link rel="manifest" href="/site.webmanifest" /> */}
       </Head>
 
       <body>
